330bad7958a204bc8e4e87625ff344395c936fd6
cscscscscscscscscscs <krivodanovaksu@gmail.com>
2023-10-27 13:48:57 +0300

    lab 1.6

--- src/main/java/com/example/ksushalab310/HelloApplication.java
+++ src/main/java/com/example/ksushalab310/HelloApplication.java
@@ -3,16 +3,19 @@ package com.example.ksushalab310;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 900, 600);
-        stage.setTitle("Лабораторная 1.5");
+        stage.setTitle("Лабораторная 1.6");
+        stage.getIcons().add(new Image(Objects.requireNonNull(HelloApplication.class.getResourceAsStream("/com/example/ksushalab310/images/logo.png"))));
         stage.setScene(scene);
         stage.show();
     }
