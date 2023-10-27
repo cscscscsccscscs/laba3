b3298b4cb850c53a442da1010eebe7096e998d2b
cscscscscscscscscscs <krivodanovaksu@gmail.com>
2023-10-27 13:09:03 +0300

    lab 1.4

---
+++ src/main/java/com/example/ksushalab310/Groups.java
@@ -0,0 +1,62 @@
+package com.example.ksushalab310;
+
+public class Groups {
+    String id, faculty, group, course, count_stud, count_group;
+
+    public Groups(String id, String faculty, String group, String course, String count_stud, String count_group) {
+        this.id = id;
+        this.faculty = faculty;
+        this.group = group;
+        this.course = course;
+        this.count_stud = count_stud;
+        this.count_group = count_group;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public String getCount_stud() {
+        return count_stud;
+    }
+
+    public String getCount_group() {
+        return count_group;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public void setCount_stud(String count_stud) {
+        this.count_stud = count_stud;
+    }
+
+    public void setCount_group(String count_group) {
+        this.count_group = count_group;
+    }
+}
--- src/main/java/com/example/ksushalab310/HelloController.java
+++ src/main/java/com/example/ksushalab310/HelloController.java
@@ -1,14 +1,47 @@
 package com.example.ksushalab310;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.text.Font;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TableColumn<Groups, String> column_count_group, column_count_stud,
+            column_course, column_group, column_num, column_faculty;
+
+    @FXML
+    private Label l_count_group, l_count_stud, l_course, l_faculty, l_group,l_num;
+
+    @FXML
+    private TableView<Groups> tableview;
+
+    ObservableList<Groups> groups = FXCollections.observableArrayList(
+            new Groups("1", "КТиИБ", "ИСТ-331", "3", "17",
+                    "2"),
+            new Groups("1", "КТиИБ", "ИСТ-332", "3", "20",
+                    "2"),
+            new Groups("1", "КТиИБ", "ПИ-332", "3", "19",
+                    "2"));
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        column_num.setCellValueFactory(new PropertyValueFactory<Groups, String>("id"));
+        column_faculty.setCellValueFactory(new PropertyValueFactory<Groups, String>("faculty"));
+        column_group.setCellValueFactory(new PropertyValueFactory<Groups, String>("group"));
+        column_course.setCellValueFactory(new PropertyValueFactory<Groups, String>("course"));
+        column_count_stud.setCellValueFactory(new PropertyValueFactory<Groups, String>("count_stud"));
+        column_count_group.setCellValueFactory(new PropertyValueFactory<Groups, String>("count_group"));
+
+        tableview.setItems(groups);
     }
 }
